680e4ad32841360a83225648a469907bab172444
VIV-PC <vivianezeobi.o@gmail.com>
2025-11-05 18:23:17 +0100

    new record

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,26 @@
 //   plugins: [],
 // }
 
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.html"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: [
+    "./*.html",       // HTML in root
+    "./**/*.html",    // HTML in subfolders
+    "./src/**/*.js",  // JS in your source folder
+    "./js/**/*.js"    // or JS in a 'js' folder if you don't have src
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
 
